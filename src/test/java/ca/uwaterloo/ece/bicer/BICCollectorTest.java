296ff3880dd1a6b02d70767b13a838234f021540
lifove <jc@lifove.net>
2016-07-14 18:13:59 -0400

    Update BICCollector test for jacrabbit

--- src/test/java/ca/uwaterloo/ece/bicer/BICCollectorTest.java
+++ src/test/java/ca/uwaterloo/ece/bicer/BICCollectorTest.java
@@ -14,11 +14,11 @@ public class BICCollectorTest {
     	BICCollector runner = new BICCollector();
     	
     	//String [] args ={"-d","data/exampleBIChanges.txt", "-g", System.getProperty("user.home") + "/git/BICER"};
-    	String [] args ={"-g",System.getProperty("user.home") + "/Documents/ODP/projects/lucene/git",
-    						"-s", "2010-09-17 00:00:00",
-    						"-e", "2011-06-29 23:59:59",
-    						"-l", "2013-01-16 23:59:59",
-    						"-b", System.getProperty("user.home") + "/Documents/ODP/projects/lucene/lucene_bug_reports.txt"};
+    	String [] args ={"-g",System.getProperty("user.home") + "/Documents/ODP/projects/jackrabbit/git",
+    						"-s", "2007-09-12 00:00:00", // "-s", "2010-09-17 00:00:00",
+    						"-e", "2009-05-29 23:59:59", // "-e", "2011-06-29 23:59:59",
+    						"-l", "2013-01-14 23:59:59", // "-l", "2013-01-16 23:59:59"
+    						"-b", System.getProperty("user.home") + "/Documents/ODP/projects/jackrabbit/jackrabbit_bug_reports.txt"};
     	
     	runner.run(args);
     	
