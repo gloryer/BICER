eb65eb72dede7de79f1c82c07f816e957ee17a30
lifove <jc@lifove.net>
2016-07-15 16:55:42 -0400

    Update BICColelctor test for correct end date

--- src/test/java/ca/uwaterloo/ece/bicer/BICCollectorTest.java
+++ src/test/java/ca/uwaterloo/ece/bicer/BICCollectorTest.java
@@ -13,23 +13,23 @@ public class BICCollectorTest {
         
     	BICCollector runner = new BICCollector();
     	
-    	String project="lucene";
+    	String project="jackrabbit";
     	
     	//String [] args ={"-d","data/exampleBIChanges.txt", "-g", System.getProperty("user.home") + "/git/BICER"};
-    	String [] args ={"-g",System.getProperty("user.home") + "/Documents/UW/ODP/projects/" + project + "/git/git",
+    	String [] args ={"-g",System.getProperty("user.home") + "/Documents/ODP/projects/" + project + "/git",
     						// jackrabbit
-    						/*"-s", "2007-09-12 00:00:00",
-    						"-e", "2009-05-29 23:59:59",
-    						"-l", "2013-01-14 23:59:59",*/
+    						"-s", "2007-09-12 00:00:00",
+    						"-e", "2009-09-14 23:59:59",
+    						"-l", "2013-01-14 23:59:59",
     						//lucene
-    						"-s", "2010-09-17 00:00:00",
+    						/*"-s", "2010-09-17 00:00:00",
     						"-e", "2011-06-29 23:59:59",
-    						"-l", "2013-01-16 23:59:59",
+    						"-l", "2013-01-16 23:59:59",*/
     						//test
 			    			/*"-s", "2010-09-21 00:00:00",
 							"-e", "2010-11-05 23:59:59",
 							"-l", "2010-11-05 23:59:59",*/
-    						"-b", System.getProperty("user.home") + "/Documents/UW/ODP/projects/" + project + "/" + project + "_bug_reports.txt"};
+    						"-b", System.getProperty("user.home") + "/Documents//ODP/projects/" + project + "/" + project + "_bug_reports.txt"};
     	
     	runner.run(args);
     	
